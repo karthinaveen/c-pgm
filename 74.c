0cd1e953881b403037e2b63d74c8ea0dcf1ff52e
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-03-05 14:09:09 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float n;
+int next;
+printf("\nEnter the number");
+scanf("%f",&n);
+next=n+1;
+printf("\nThe next nearest integer :%d",next);
+}
