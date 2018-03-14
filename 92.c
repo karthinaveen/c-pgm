2f76bd7638c7bbc4bc3d4edb9cd35eaaf98501cb
karthinaveen <35688297+karthinaveen@users.noreply.github.com>
2018-03-14 15:58:37 +0530

    Create 92.c

---
+++ 92.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b[100],c=0,i;
+printf("enter the numbers:");
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+scanf("%d",&b[i]);
+}
+for(i=0;i<a;i++)
+{
+c=c+b[i];
+}
+printf("\n%d",c);	
+}
